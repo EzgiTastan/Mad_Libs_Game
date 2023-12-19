00f67f46ed6d52dfda03dda993fcb403bb4bbfb6
kedikimi <kedigunesi@gmail.com>
2023-12-19 18:08:54 +0300

    Mad Libs
    Mad Libs is a game.

---
+++ Mad Libs Game/Mad_Libs_game.py
@@ -0,0 +1,15 @@
+animal = input("Let's create a unique story. Enter an animal name: ")
+profession = input("Enter a profession name: ")
+street = input("Enter a street name: ")
+
+print("While a " + animal + " who was a " + profession + " was walking down the " + street + " Street,")
+
+animal2 = input("Enter another animal name: ")
+name = input("Enter a name: ")
+place = input("Enter the name of your favourite cafe: ")
+
+print("He saw his friend, " + animal2 + " whose name is " + name + " in his favourite cafe, " + place)
+
+adjective = input("Enter an adjective: ")
+sport = input("Enter a sport name: ")
+print( "Then, he thought about how " + adjective + " she is. She was definitely a " + sport + "-lover type, he thought.")
